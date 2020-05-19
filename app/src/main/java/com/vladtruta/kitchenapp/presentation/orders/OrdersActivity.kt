b1f9ccbfca2e2347a7045a983ca259b9a2cba372
Vlad Truta <vlad.truta@tapptitude.com>
2020-05-19 23:21:09 +0300

    Further work on RecyclerView adapters

--- app/src/main/java/com/vladtruta/kitchenapp/presentation/orders/OrdersActivity.kt
+++ app/src/main/java/com/vladtruta/kitchenapp/presentation/orders/OrdersActivity.kt
@@ -1,13 +1,47 @@
 package com.vladtruta.kitchenapp.presentation.orders
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.vladtruta.kitchenapp.R
+import com.vladtruta.kitchenapp.databinding.ActivityOrdersBinding
+import com.vladtruta.kitchenapp.model.local.KitchenOrder
+import com.vladtruta.kitchenapp.presentation.orders.adapter.OrderDetailsAdapter
+import com.vladtruta.kitchenapp.presentation.orders.adapter.OrdersListAdapter
+import com.vladtruta.kitchenapp.presentation.orders.adapter.TotalCoursesAdapter
 
-class OrdersActivity : AppCompatActivity() {
+class OrdersActivity : AppCompatActivity(), OrdersListAdapter.OrdersListListener {
+
+    private lateinit var binding: ActivityOrdersBinding
+
+    private lateinit var ordersListAdapter: OrdersListAdapter
+    private lateinit var orderDetailsAdapter: OrderDetailsAdapter
+    private lateinit var totalCoursesAdapter: TotalCoursesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityOrdersBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_orders)
+
+        initViews()
+        initObservers()
+        initActions()
+    }
+
+    private fun initViews() {
+        ordersListAdapter = OrdersListAdapter(this)
+        orderDetailsAdapter = OrderDetailsAdapter()
+        totalCoursesAdapter = TotalCoursesAdapter()
+    }
+
+    private fun initObservers() {
+
+    }
+
+    private fun initActions() {
+
+    }
+
+    override fun onOrderListItemClicked(kitchenOrder: KitchenOrder) {
+
     }
 }
